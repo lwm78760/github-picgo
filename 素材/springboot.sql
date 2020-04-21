af09b49f67748665569659fda289f9754ada6e0b
lwm78760 <2559882478@qq.com>
2020-04-21 20:51:08 +0800

    Update springboot.sql

--- 素材/springboot.sql
+++ 素材/springboot.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 21/04/2020 19:29:32
+ Date: 21/04/2020 20:49:07
 */
 
 SET NAMES utf8mb4;
@@ -41,7 +41,7 @@ INSERT INTO `department` VALUES (105, '后勤部');
 -- ----------------------------
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee`  (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `last_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `gender` int(10) NOT NULL,
@@ -55,10 +55,11 @@ CREATE TABLE `employee`  (
 -- ----------------------------
 -- Records of employee
 -- ----------------------------
-INSERT INTO `employee` VALUES (1001, 'AA', 'A24734673@qq.com', 1, '1999-05-06 00:00:00', 101);
+INSERT INTO `employee` VALUES (1001, 'AA', 'AA24736743@qq.com', 1, '1997-05-06 00:00:00', 101);
 INSERT INTO `employee` VALUES (1002, 'BB', 'B24734673@qq.com', 0, '1998-05-06 00:00:00', 102);
 INSERT INTO `employee` VALUES (1003, 'CC', 'C24734673@qq.com', 1, '2001-01-01 00:00:00', 103);
 INSERT INTO `employee` VALUES (1004, 'DD', 'D24734673@qq.com', 0, '2002-01-01 00:00:00', 104);
 INSERT INTO `employee` VALUES (1005, 'EE', 'E24734673@qq.com', 1, '2003-01-01 00:00:00', 105);
+INSERT INTO `employee` VALUES (1007, 'lwm', '24736743@qq.com', 1, '2020-04-21 20:48:01', 101);
 
 SET FOREIGN_KEY_CHECKS = 1;
